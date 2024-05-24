708c044725b1c5fa833264415e45219eb001cfd9
David Bezerra da Silva <jhonnymuitobravo@gmail.com>
2024-05-23 22:31:30 -0300

    tome

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { PhotoService } from '../services/photo.service';
+import { PokeAPIService } from '../services/poke-api.service';
+import { SharePokemonService } from '../services/share-pokemon.service';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +9,72 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  pokemonData: any = null;
+  pokemon1Data: any = null;
+  resultado: string = '';
 
-  constructor() {}
+  constructor(
+    public photoService: PhotoService,
+    private pokeAPIService: PokeAPIService,
+    private sharePokemonService: SharePokemonService
+  ) {}
+
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
+
+  ngOnInit() {
+    this.sharePokemonService.pokemon1$.subscribe(pokemonData => {
+      this.pokemon1Data = pokemonData;
+      this.fetchRandomPokemon();
+    });
+  }
+
+  fetchRandomPokemon() {
+    const pokemonId = Math.floor(Math.random() * 100) + 1;
+    this.pokeAPIService.getPokeAPIService(pokemonId).subscribe(pokemonData => {
+      this.pokemonData = pokemonData;
+      this.sharePokemonService.setPokemon2(pokemonData);
+      this.comparePokemons();
+    });
+  }
+
+  comparePokemons() {
+    if (this.pokemon1Data && this.pokemonData) {
+      const abilities1 = this.pokemon1Data.abilities.length;
+      const abilities2 = this.pokemonData.abilities.length;
+
+      if (abilities1 === abilities2) {
+        this.resultado = 'Empate';
+        this.sharePokemonService.incrementDraw(this.pokemon1Data.id);
+      } else if (abilities1 > abilities2) {
+        this.resultado = 'Ganhou';
+        this.sharePokemonService.incrementVictory(this.pokemon1Data.id);
+      } else {
+        this.resultado = 'Perdeu';
+        this.sharePokemonService.incrementDefeat(this.pokemon1Data.id);
+      }
+    }
+  }
+
+  get resultadoColor() {
+    switch (this.resultado) {
+      case 'Empate':
+        return 'yellow';
+      case 'Ganhou':
+        return 'red';
+      case 'Perdeu':
+        return 'green';
+      default:
+        return '';
+    }
+  }
+
+  get pokemonImageUrl() {
+    if (this.pokemonData) {
+      return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.pokemonData.id}.png`;
+    }
+    return ''; 
+  }
 
 }
